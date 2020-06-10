8742be0d6563ca00de65a92de7c304a233dd345b
jingkongming <309982451@qq.com>
2020-06-10 10:58:30 +0800

    lbyssh2

---
+++ ssh/src/ssh/Lby.java
@@ -0,0 +1,21 @@
+package ssh;
+
+public class Lby {
+
+public void addUser(int id, String usrename , String password)
+{//添加操作
+
+}
+
+public void deleteUser(int id)
+{//删除操作
+
+}
+
+public void updateUser(int id, String newUsername, String newPassword) 
+{//修改操作
+
+	
+}
+
+}
